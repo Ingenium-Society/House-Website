b20c6949a206af3999de299de4a201848f3f283d
Dadarzz2405 <haidaralifawwaz@gmail.com>
2026-09-21 21:01:42 +0700

    change test.py to main.py

--- backend/main.py
+++ backend/main.py
@@ -4,7 +4,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-
+# Define a route for the API and data sent to the frontend
 @app.get("/api/hello")
 def hello():
     return jsonify({
